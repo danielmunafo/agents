3fb52c6c47ba8bbf1b25ec272cfe3d17dbded800
Daniel Munafó Tostes <dann.tostes@gmail.com>
2025-11-23 00:34:33 +0100

    feat: add scaffolding

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import http from "node:http";
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
+
+export { server };
